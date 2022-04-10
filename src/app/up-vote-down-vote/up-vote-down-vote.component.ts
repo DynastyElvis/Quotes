584b802e0045510a4acae5994f5b0b4f69cdaa2c
DynastyElvis <kipkemoi.rono@student.moringaschool.com>
2022-04-11 01:34:43 +0300

    bundle commits

---
+++ src/app/up-vote-down-vote/up-vote-down-vote.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+import { Quotes } from '../quotes';
+
+@Component({
+  selector: 'app-up-vote-down-vote',
+  templateUrl: './up-vote-down-vote.component.html',
+  styleUrls: ['./up-vote-down-vote.component.css']
+})
+export class UpVoteDownVoteComponent implements OnInit {
+ 
+  numberOfUpvotesAndDownVotes: number = 0;
+
+  upVoteButtonClick() {
+    this.numberOfUpvotesAndDownVotes++;
+    
+  }
+  downVoteButtonClick(){
+    this.numberOfUpvotesAndDownVotes--;
+  }
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
